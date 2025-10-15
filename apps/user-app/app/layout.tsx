fdb6b948b88c11319e093b4ead8293f2741a8482
Tahir Aziz Khan <tahirazizkhan02@gmail.com>
2025-10-15 15:57:33 +0530

    biGFM

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",         
     shortcut: "/favicon.ico",   
-    apple: "/apple-touch-icon.png",
+    apple: "/favicon.ico",
   },
   openGraph: {
     title: "OKANE - Your Digital Wallet",
