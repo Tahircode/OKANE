95fc7f333728a1e4480428bf251654df2eb490b6
Tahir Aziz Khan <tahirazizkhan02@gmail.com>
2025-10-17 18:51:15 +0530

    new

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3001" , "https://okane-1.onrender.com"], 
+  origin: ["http://localhost:3001" , "https://okane.vercel.app"], 
   methods: ["GET", "POST"],
   }
 ))
